55c9102af6dd70cc52a62e5a47c9111e2894f8ed
jorgsaul <jvelazquezp2301@alumno.ipn.mx>
2025-10-06 23:49:34 -0600

    creacion de la logica en escritura

--- App.js
+++ App.js
@@ -1,40 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [input, setInput] = useState('')
+  
+  const presionar = valor =>{
+    setInput(input + valor)
+  }
+
+  const borrar = () =>{
+    setInput('')
+  }
+
   const Boton = ({texto , presionado}) => (
-    <TouchableOpacity style={styles.boton}>
+    <TouchableOpacity style={styles.boton} onPress={presionado}>
       <Text>{texto}</Text>
     </TouchableOpacity>
   )
 
   return (
     <View style={styles.container}>
-      <Text style={styles.input}>0</Text>
+      <Text style={styles.input}>{input || '0'}</Text>
       <Text style={styles.resultado}> Resultado </Text>
       <View style={styles.fila}>
-        <Boton texto= '7'/>
-        <Boton texto= '8'/>
-        <Boton texto= '9'/>
-        <Boton texto= '+'/>
+        <Boton texto= '7' presionado={()=>presionar('7')}/>
+        <Boton texto= '8' presionado={()=>presionar('8')}/>
+        <Boton texto= '9' presionado={()=>presionar('9')}/>
+        <Boton texto= '+' presionado={()=>presionar('+')}/>
       </View>
       <View style={styles.fila}>
-        <Boton texto= '4'/>
-        <Boton texto= '5'/>
-        <Boton texto= '6'/>
-        <Boton texto= '-'/>
+        <Boton texto= '4' presionado={()=>presionar('4')}/>
+        <Boton texto= '5' presionado={()=>presionar('5')}/>
+        <Boton texto= '6' presionado={()=>presionar('6')}/>
+        <Boton texto= '-' presionado={()=>presionar('-')}/>
       </View>
       <View style={styles.fila}>
-        <Boton texto= '1'/>
-        <Boton texto= '2'/>
-        <Boton texto= '3'/>
-        <Boton texto= '*'/>
+        <Boton texto= '1' presionado={()=>presionar('1')}/>
+        <Boton texto= '2' presionado={()=>presionar('2')}/>
+        <Boton texto= '3' presionado={()=>presionar('3')}/>
+        <Boton texto= '*' presionado={()=>presionar('*')}/>
       </View>
       <View style={styles.fila}>
-        <Boton texto= '0'/>
-        <Boton texto= 'C'/>
-        <Boton texto= '='/>
-        <Boton texto= '/'/>
+        <Boton texto= '0' presionado={()=>presionar('0')}/>
+        <Boton texto= 'C' presionado={borrar}/>
+        <Boton texto= '=' />
+        <Boton texto= '/' presionado={()=>presionar('/')}/>
       </View>
       <StatusBar style="auto" />
     </View>
